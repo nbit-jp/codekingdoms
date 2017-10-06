88680ccda680447f8f45f5d63c1c115c215feed5
Seiji Irie <seiji.irie@gmail.com>
2017-10-06 22:07:32 +0900

    初期コード

---
+++ Hungry Games/Game.java
@@ -0,0 +1,8 @@
+package space.codekingdoms.nbituser.myswiftgames;
+
+import com.codekingdoms.nozzle.base.BaseGame;
+
+public class Game extends BaseGame {
+	
+	
+}
---
+++ Hungry Games/Player.java
@@ -0,0 +1,8 @@
+package space.codekingdoms.nbituser.myswiftgames;
+
+import com.codekingdoms.nozzle.base.BasePlayer;
+
+public class Player extends BasePlayer {
+	
+	
+}
