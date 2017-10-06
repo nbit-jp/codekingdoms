8c72f80ef906ab364587f634f86261a0a7f772a8
Seiji Irie <seiji.irie@gmail.com>
2017-10-06 22:57:23 +0900

    Step2.3 broadcastMessageの追加

--- Hungry Games/Game.java
+++ Hungry Games/Game.java
@@ -14,20 +14,23 @@ public class Game extends BaseGame {
 		world.setPVP(false);
 		disableMobSpawning();
 		gamePhase = 1;
+		broadcastMessage("Game starts in 30 seconds...");
 		
 	}
 	
 	public void onTimerExpire() {
 	
-		if(gamePhase == 1) {
+		if (gamePhase == 1) {
 			
 			createWorldBorder(500);
 			gamePhase = 2;
 			startTimer(10);
+			broadcastMessage("Go! PVP starrts in 10 seconds!");
 			
 		} else if (gamePhase == 2) {
 			
 			world.setPVP(true);
+			broadcastMessage("PVP is started! Fight!");
 			gamePhase = 3;
 			
 		}
