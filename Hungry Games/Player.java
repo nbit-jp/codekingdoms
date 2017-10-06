d6e2eb8677f2e02806e1f0e751e067a4a127c3c6
Seiji Irie <seiji.irie@gmail.com>
2017-10-06 23:15:13 +0900

    Step2.4 ゲーム中の時は参加させない

--- Hungry Games/Player.java
+++ Hungry Games/Player.java
@@ -4,6 +4,16 @@ import com.codekingdoms.nozzle.base.BasePlayer;
 
 public class Player extends BasePlayer {
 	
+	public void onJoin() {
+		
+		if  (getGame().gamePhase > 1) {
+			
+			kickPlayer("There is already a game in progress!");
+
+		}	
+
+	}	
+
 	public void onDeath() {
 		
 		kickPlayer("You died!");
