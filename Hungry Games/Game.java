67d0cb3c25e20498af33319f91619e06e42022bf
Seiji Irie <seiji.irie@gmail.com>
2017-10-06 22:38:08 +0900

    Step1.2 WorldBorderの追加、タイマーの設定など

--- Hungry Games/Game.java
+++ Hungry Games/Game.java
@@ -4,5 +4,19 @@ import com.codekingdoms.nozzle.base.BaseGame;
 
 public class Game extends BaseGame {
 	
+	public void onCodeUpdate() {
+		
+		createWorldBorder(6);
+		setWorldBorderCenter(new Location(world, 140.53, 72, 242.64));
+		startTimer(30);
+		world.setPVP(false);
+		disableMobSpawning();
+	}
 	
+	public void onTimerExpire() {
+		
+		world.setPVP(true);
+		createWorldBorder(500);
+	}
+			
 }
