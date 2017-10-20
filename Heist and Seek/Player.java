cd2b487b3327dfce228b604fccbf1214b8088ce5
Seiji Irie <seiji.irie@gmail.com>
2017-10-20 22:12:54 +0900

    Step1.1 scoreの導入、ステンドグラスを壊したらスコア加算

--- Heist and Seek/Player.java
+++ Heist and Seek/Player.java
@@ -1,8 +1,12 @@
 package space.codekingdoms.nbituser.mylazyheist;
 
 import com.codekingdoms.nozzle.base.BasePlayer;
+import org.bukkit.block.Block;
+import org.bukkit.Material;
 
 public class Player extends BasePlayer {
+	
+	public int score;
 	
 	public void onJoin() {
 		
@@ -16,6 +20,18 @@ public class Player extends BasePlayer {
 			
 		
 		, 1);
+	
+	}
+	
+	public void onMine( Block block ) {
+		
+		if (block.getType().equals(Material.STAINED_GLASS)) {
+			
+			score = score + 1;
+			getGame().broadcastMessage( name + " got a box, now has " + score );
+			
+		}
+		
 	
 	}
 	
