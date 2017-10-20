fa87adb0e51f67c9c9418f345eeb701255544a78
Seiji Irie <seiji.irie@gmail.com>
2017-10-20 22:31:58 +0900

    Step1.2 isHiderで隠すプレイヤーを決定

--- Heist and Seek/Game.java
+++ Heist and Seek/Game.java
@@ -16,8 +16,32 @@ public class Game extends BaseGame {
 		world.setPVP(false);
 		jail = new Location(world, 856.83, 5, 114.38);
 		outside = new Location(world, 852.39, 4, 110.33);
+		startHiding();
 	
 	}
 	
+	public void startHiding() {
+		
+		if ( getPlayerList().length == 0 ) {
+			
+			broadcastMessage("No players on your server, game stopping.");
+			return;
+		}
+
+		Player chosenPlayer = getPlayerList() [0];
+
+		for ( Player player : getPlayerList()) {
+			if (player.equals( chosenPlayer )) {
+				
+				player.isHider = true;
+				player.sendMessage("You're hiding stuff!");
+				
+			} else {
+				
+				player.isHider = false;
+
+			}
+		}
+	}
 	
 }
