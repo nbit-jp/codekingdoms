6ac2a8a6ae358b8452708ced2903cc2061b77b19
Seiji Irie <seiji.irie@gmail.com>
2017-11-01 22:54:40 +0900

    Step5.1 プレイヤー出現位置の定義

--- Hungry Games/Game.java
+++ Hungry Games/Game.java
@@ -3,15 +3,26 @@ package space.codekingdoms.nbituser.myswiftgames;
 import org.bukkit.World;
 import org.bukkit.Location;
 import com.codekingdoms.nozzle.base.BaseGame;
+import org.bukkit.GameMode;
 
 public class Game extends BaseGame {
 	
 	public int gamePhase;
 	Location centerStage = new Location(world, 140.53, 72, 242.64);
+	public Location[] spawnPoints;
 
 	public void onCodeUpdate() {
 		
-		createWorldBorder(6);
+		spawnPoints = new Location[8];
+		spawnPoints[0] = new Location(world, 140.52, 72 ,224.24);
+		spawnPoints[1] = new Location(world, 140.51, 72 ,260.66);
+		spawnPoints[2] = new Location(world, 158.55, 72 ,242.53);
+		spawnPoints[3] = new Location(world, 122.2 , 72 ,242.31);
+		spawnPoints[4] = new Location(world, 153.86, 72 ,255.43);
+		spawnPoints[5] = new Location(world, 127.28, 72 ,229.5);
+		spawnPoints[6] = new Location(world, 127.6,  72 ,255.64);
+		spawnPoints[7] = new Location(world, 153.48, 72 ,229.33);
+		
 		setWorldBorderCenter(centerStage);
 		startTimer(30);
 		world.setPVP(false);
@@ -21,32 +32,32 @@ public class Game extends BaseGame {
 		for (Player player : getPlayerList()) {
 			
 			player.startGame();
+			player.teleport(spawnPoints[0]);
 			
-		}		
-
+		}
+		
 	}
 	
 	public void onTimerExpire() {
-	
-		gamePhase = gamePhase + 1;
 		
+		gamePhase = gamePhase + 1;
+
 		switch (gamePhase) {
 			
 			case 2:
-
-				createWorldBorder(500);
+				
 				gamePhase = 2;
 				startTimer(10);
 				broadcastMessage("Go! PVP starrts in 10 seconds!");
 				break;
-			
+							
 			case 3:
-			
+				
 				world.setPVP(true);
 				broadcastMessage("PVP is started! Fight!");
 				startTimer(600);
 				break;
-			
+				
 			case 4:
 				
 				expandWorldBorder(-450, 120);
@@ -62,23 +73,23 @@ public class Game extends BaseGame {
 	public void checkGameOver() {
 		
 		int playerCount = 0;
-
+		
 		for (Player player : getPlayerList()) {
-
+			
 			if (player.getGameMode() == GameMode.ADVENTURE) {
-
+				
 				playerCount = playerCount + 1;
 				
 			}
-						
+			
 		}
 		
 		if (playerCount < 2) {
-				
+			
 			startTimer(10);
 			broadcastTitle("You won!", "May the odds be ever in your favour!");
 			gamePhase = 4;
-				
+			
 		}
 	}
 }
