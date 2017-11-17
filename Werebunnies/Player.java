c20af5af6659f8df014682e642ded2d4fe897369
Seiji Irie <seiji.irie@gmail.com>
2017-11-17 23:50:55 +0900

    Step2.1 joinしたら卵を装備

--- Werebunnies/Player.java
+++ Werebunnies/Player.java
@@ -1,8 +1,20 @@
 package space.codekingdoms.nbituser.mybrainybunnies;
 
 import com.codekingdoms.nozzle.base.BasePlayer;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 public class Player extends BasePlayer {
 	
+	public void equip() {
+		
+		addItemToInventory(new ItemStack(Material.EGG, 8));
 	
+	}
+	
+	public void onJoin() {
+		
+		equip();
+		
+	}
 }
