4dc6de623cfb77ec3d573517a0bbae9d53aa8e91
Seiji Irie <seiji.irie@gmail.com>
2017-11-17 23:44:32 +0900

    初期コード

---
+++ Werebunnies/Game.java
@@ -0,0 +1,22 @@
+package space.codekingdoms.nbituser.mybrainybunnies;
+
+import com.codekingdoms.nozzle.base.BaseGame;
+import com.codekingdoms.nozzle.utils.Random;
+
+public class Game extends BaseGame {
+	
+	public Player getRandomPlayer() {
+		
+		Player[] array = getPlayerList();
+		if (array.length == 0) {
+			
+			return null;
+			
+		}
+		
+		return array[Random.generateInteger(0, array.length - 1)];
+	
+	}
+	
+	
+}
---
+++ Werebunnies/Player.java
@@ -0,0 +1,8 @@
+package space.codekingdoms.nbituser.mybrainybunnies;
+
+import com.codekingdoms.nozzle.base.BasePlayer;
+
+public class Player extends BasePlayer {
+	
+	
+}
