84173b0f08d11b7dad6d2a388a2aa528fb8822ba
Seiji Irie <seiji.irie@gmail.com>
2017-12-01 21:45:12 +0900

    Step3.1 Factionを追加

--- Werebunnies/Game.java
+++ Werebunnies/Game.java
@@ -5,6 +5,8 @@ import com.codekingdoms.nozzle.utils.Random;
 
 public class Game extends BaseGame {
 	
+	public Faction humans;
+	
 	public Player getRandomPlayer() {
 		
 		Player[] array = getPlayerList();
@@ -16,14 +18,21 @@ public class Game extends BaseGame {
 		
 		return array[Random.generateInteger(0, array.length - 1)];
 	
-    }
-    
-    public void onCodeUpdate() {
+	}
+	
+	public void startRound() {
+		
+		humans = new Faction();
+		
+	}
+	
+	public void onCodeUpdate() {
 		
 		disableMobSpawning();
 		createWorldBorder(50);
 		world.setTime(13000);
+		startRound();
 		
 	}
-	
+		
 }
