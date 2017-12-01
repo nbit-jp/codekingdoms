65ca8145b5718cf34ec4a1d01993ebe67c885f5c
Seiji Irie <seiji.irie@gmail.com>
2017-12-01 21:38:08 +0900

    Step2.2 onProjectileHitTargetメソッドを追加

--- Werebunnies/Player.java
+++ Werebunnies/Player.java
@@ -3,6 +3,7 @@ package space.codekingdoms.nbituser.mybrainybunnies;
 import com.codekingdoms.nozzle.base.BasePlayer;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.Location;
 
 public class Player extends BasePlayer {
 	
@@ -15,6 +16,13 @@ public class Player extends BasePlayer {
 	public void onJoin() {
 		
 		equip();
-		
+	
 	}
+	
+	public void onProjectileHitTarget( String projectileType, Location hitZone ) {
+		
+		world.createExplosion(hitZone, 3f, true);
+	
+	}
+		
 }
