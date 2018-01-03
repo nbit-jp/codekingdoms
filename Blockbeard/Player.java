002abb0d7696718a5bde7b538e898ff447a3174d
Seiji Irie <seiji.irie@gmail.com>
2018-01-03 19:00:21 +0900

    Step2.2 spawn地点を決める

--- Blockbeard/Player.java
+++ Blockbeard/Player.java
@@ -4,5 +4,22 @@ import com.codekingdoms.nozzle.base.BasePlayer;
 
 public class Player extends BasePlayer {
 	
+	public void spawnPlayer() {
+		
+		teleport(getGame().getRandomBlockAtHeight(0));
+
+	}
+
+	public void onJoin() {
+		
+		spawnPlayer();
+		
+	}
+	
+	public void onRespawn() {
+		
+		spawnPlayer();
+		
+	}
 	
 }
