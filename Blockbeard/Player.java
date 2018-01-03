adbcd679db1b72b9572b3e7b9190a0a1bd330320
Seiji Irie <seiji.irie@gmail.com>
2018-01-03 22:57:50 +0900

    Step4.2 コンパスの向きを指定

--- Blockbeard/Player.java
+++ Blockbeard/Player.java
@@ -14,6 +14,7 @@ public class Player extends BasePlayer {
 		addItemToInventory(new ItemStack(Material.IRON_PICKAXE));
 		addItemToInventory(new ItemStack(Material.IRON_SPADE));
 		addItemToInventory(new ItemStack(Material.COMPASS));
+		setCompassTarget(getGame().chestPosition);
 	
 	}
 	
