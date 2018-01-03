6f46347ab15bfb0648860ee898aa7d563d91c2c8
Seiji Irie <seiji.irie@gmail.com>
2018-01-03 18:41:07 +0900

    Step2.1 chestPositionを導入

--- Blockbeard/Game.java
+++ Blockbeard/Game.java
@@ -3,38 +3,41 @@ package space.codekingdoms.nbituser.mysmallisland;
 import com.codekingdoms.nozzle.base.BaseGame;
 import com.codekingdoms.nozzle.utils.Random;
 import org.bukkit.Location;
+import org.bukkit.Material;
 
 public class Game extends BaseGame {
 	
 	int radius = 50;
 	
 	public void onCodeUpdate() {
-	
+		
 		startTimer(300);
 		createWorldBorder(radius*2);
 		startRound();
-		
+	
 	}
 	
 	public void startRound() {
 		
-		broadcastMessage("Round started!");
-		
+		Location chestPosition = getRandomBlockAtHeight(-3);
+		broadcastMessage(chestPosition.toString());
+		setBlockTypeAtLocation(Material.EMERALD_BLOCK, chestPosition);
+	
 	}
 	
 	public void endGame() {
 		
 		broadcastMessage("The game is over!");
 		resetGame();
-		
+	
 	}
 	
 	public void onTimerExpire() {
-
+		
 		endGame();
-
+	
 	}
-
+	
 	public Location getRandomBlockAtHeight( int yOffest ) {
 		
 		Location spawnLocation = world.getSpawnLocation();
