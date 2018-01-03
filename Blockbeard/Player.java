79ac0c07fcf5d3cdb09cd462b3de97dd87f595c2
Seiji Irie <seiji.irie@gmail.com>
2018-01-03 22:52:48 +0900

    Step4.1 spawn時のインベントリ追加

--- Blockbeard/Player.java
+++ Blockbeard/Player.java
@@ -1,26 +1,34 @@
 package space.codekingdoms.nbituser.mysmallisland;
 
 import com.codekingdoms.nozzle.base.BasePlayer;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 public class Player extends BasePlayer {
 	
 	public void spawnPlayer() {
 		
+		clearInventory();
 		teleport(getGame().getRandomBlockAtHeight(0));
-
+		addItemToInventory(new ItemStack(Material.IRON_SWORD));
+		addItemToInventory(new ItemStack(Material.IRON_PICKAXE));
+		addItemToInventory(new ItemStack(Material.IRON_SPADE));
+		addItemToInventory(new ItemStack(Material.COMPASS));
+	
 	}
-
+	
 	public void onJoin() {
 		
 		spawnPlayer();
-		
+	
 	}
 	
 	public void onRespawn() {
 		
 		spawnPlayer();
 		setBedSpawnLocation(getGame().getRandomBlockAtHeight(0));
-		
+	
 	}
+	
 	
 }
